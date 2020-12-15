7b18fb6e85df78a0154ece372e93339d9135c80c
Vashar20 <76012469+Vashar20@users.noreply.github.com>
2020-12-16 01:27:40 +0330

    Update F_Geneapi.py
    Declaration of code was added.

--- F_Geneapi.py
+++ F_Geneapi.py
@@ -13,21 +13,24 @@ def get_gene():
         # Optional parameter   
         species = request.args.get('species')
         # Database connection
-        db_connect = create_engine('mysql+pymysql://anonymous:@ensembldb.ensembl.org/ensembl_website_97') #where add port?
+        db_connect = create_engine('mysql+pymysql://anonymous:@ensembldb.ensembl.org:3306/ensembl_website_97')
         conn = db_connect.connect()
         # Checking lookup length
         if len(gene) >= 3:
             # Without provided species parameter
+            # This part could be rewritten using combination of query and filter if more request parameters would be considered.
             if species is None:
                 query = conn.execute("select display_label, location, stable_id, species from gene_autocomplete where display_label LIKE '%s' "  % (gene+'%%'))
                 result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}  #or need simple text results?
                 return jsonify(result)
             # With provided species parameter
+            # A list of full scientific name of all species in database could be provided to improve scpecies parameter assertion and correction.
             else:
                 query = conn.execute("select display_label, location, stable_id, species from gene_autocomplete where display_label LIKE '%s' AND species = '%s' "  % (gene+'%%',species)) 
-                result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}  #or need simple text results?
+                result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
                 return jsonify(result)
         else:
+            # All Errors could be written using decorated errors flag in Flask.
             return '''
             Error 400 HTTP code.
             Lookup (gene) length should not be less than 3 characters
